ba96b8c27ec701853d645af9ebaad22c7fe89eef
prakhar <201951116@iiitvadodara.ac.in>
2020-07-23 19:49:25 +0530

    1st

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql')
+
+
+const app = express();
+const events = []
+
+app.use(bodyParser.json()); //for incoming json data
+
+app.use('/graphql',graphqlHTTP({   //only one end point i.e graphql
+    schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+    type RootQuery {
+        events: [Event!]!
+
+    }
+
+    type RootMutation {
+        createEvent(eventInput: EventInput ): Event
+
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random.toString(),
+                title:args.eventInput.title,
+                description: args.eventInput.price,
+                date: args.eventInput.date
+            };
+            events.push(event);
+            return event;
+        }
+    },  //allthe resolvers in it
+    graphiql: true
+}))
+
+app.listen(3000);
